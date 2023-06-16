c14dcfff655ed8ee67ad1e76c3499644ab947c53
taflur <melhaouisoukaina1@gmail.com>
2023-06-16 22:03:16 +0100

    client details by soukaina

--- src/app/components/client-details.component.ts
+++ src/app/components/client-details.component.ts
@@ -4,18 +4,8 @@ import { ClientService } from '../services/client.service';
 
 @Component({
   selector: 'app-client-detail',
-  template: `
-    <h2>Client Details</h2>
-    <div *ngIf="client">
-      <p>Client ID: {{ client.id }}</p>
-      <p>Client MAC: {{ client.mac }}</p>
-      <p>Client Name: {{ client.nom }}</p>
-      <p>Client Surname: {{ client.prenom }}</p>
-      <p>Client Phone: {{ client.tel }}</p>
-      <p>Client Address: {{ client.adresse }}</p>
-      <p>Client Email: {{ client.mail }}</p>
-    </div>
-  `
+  templateUrl: './client-details.component.html',
+  styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailComponent implements OnInit {
   client: any;
