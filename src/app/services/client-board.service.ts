8d1a32627316c37f069a696d07f1bfb1b1be4237
unknown <chaimae.bousaid@etu.uae.ac.ma>
2023-06-18 14:13:17 +0100

    chaimaee

--- src/app/services/client-board.service.ts
+++ src/app/services/client-board.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -7,5 +6,17 @@ export class ClientBoardService {
   public nom!: string;
   public prenom!: string;
 
-  constructor() { }
+  constructor() {
+    this.loadFromLocalStorage(); // Charger les données depuis le stockage local lors de l'initialisation du service
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('nom', this.nom);
+    localStorage.setItem('prenom', this.prenom);
+  }
+
+  loadFromLocalStorage() {
+    this.nom = localStorage.getItem('nom') || '';
+    this.prenom = localStorage.getItem('prenom') || '';
+  }
 }
